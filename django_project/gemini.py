d24f529a666c4493b1bec7f70f70bddc27f37f71
birdideas <birdideasblog@gmail.com>
2026-03-28 17:42:09 -0400

    Sample response to save money on Gemini tokens

--- django_project/gemini.py
+++ django_project/gemini.py
@@ -17,7 +17,45 @@ Output in the following JSON format:
 ]
 """
 
+hardcoded_sample_response = """
+```json
+[
+  {
+    "degree": 1,
+    "person_a": "John Lennon",
+    "connection": "Met and collaborated with",
+    "person_b": "Yoko Ono"
+  },
+  {
+    "degree": 2,
+    "person_a": "Yoko Ono",
+    "connection": "Was photographed with and attended events with",
+    "person_b": "Andy Warhol"
+  },
+  {
+    "degree": 3,
+    "person_a": "Andy Warhol",
+    "connection": "Met and created art featuring",
+    "person_b": "Steve Jobs"
+  },
+  {
+    "degree": 4,
+    "person_a": "Steve Jobs",
+    "connection": "Regularly met with and collaborated with",
+    "person_b": "Larry Page"
+  },
+  {
+    "degree": 5,
+    "person_a": "Larry Page",
+    "connection": "Has had numerous public and private meetings with, often related to technology and future ventures, as co-founders of major tech companies",
+    "person_b": "Elon Musk"
+  }
+]
+```
+"""
+
 def send_prompt(person_a : str, person_b : str):
+    return hardcoded_sample_response
     client = genai.Client(api_key=settings.GOOGLE_API_KEY)
 
     config = types.GenerateContentConfig(
